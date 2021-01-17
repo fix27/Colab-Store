e361f54ed5bd5d62f7b7342c3de38a7e6dc4487b
Поздняков Константин <fix@inbox.ru>
2021-01-17 11:44:44 +0500

    Update short_int_triangle.py

--- short_int_triangle.py
+++ short_int_triangle.py
@@ -1,9 +1,9 @@
 import math
 def is_square(integerx):
- return integerx == int(math.sqrt(integerx if integerx>1 else 3) + 0.5) ** 2
-for a in range(1,500) :
- for b in range(1,1000) :
-  for c in range(1,4000) :
+ return  (False if integerx<4  else (integerx == int(math.sqrt(integerx) + 0.5) ** 2))
+for a in range(156,20000) :
+ for b in range(a,42000) :
+  for c in range(b,90000) :
     p=(a+b+c)/2
-    if is_square(p*(p-a)*(p-b)*(p-c)) and is_square(2*b**2+2*c**2-a**2) and is_square(2*a**2+2*c**2-b**2) and is_square(2*a**2+2*b**2-c**2) :
-       print (a,b,c,2*b**2+2*c**2-a**2,2*a**2+2*c**2-b**2,2*a**2+2*b**2-c**2,p*(p-a)*(p-b)*(p-c))
+    if is_square(p*(p-a)*(p-b)*(p-c)) and is_square(2*b**2+2*c**2-a**2)  and is_square(2*a**2+2*c**2-b**2) and is_square(2*a**2+2*b**2-c**2) :
+     print(a,b,c,(math.sqrt(2*b**2+2*c**2-a**2))/2,(math.sqrt(2*a**2+2*c**2-b**2))/2,(math.sqrt(2*a**2+2*b**2-c**2))/2,math.sqrt((p*(p-a)*(p-b)*(p-c))))
