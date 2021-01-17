ccc14c2468792b19fbc018e7317951f5b951b94a
Поздняков Константин <fix@inbox.ru>
2021-01-17 05:04:09 +0500

    Update short_int_triangle.py

--- short_int_triangle.py
+++ short_int_triangle.py
@@ -2,8 +2,8 @@ import math
 def is_square(integerx):
  return integerx == int(math.sqrt(integerx if integerx>1 else 3) + 0.5) ** 2
 for a in range(1,500) :
- for b in range(1,500) :
-  for c in range(1,500) :
+ for b in range(1,700) :
+  for c in range(1,1500) :
     p=(a+b+c)/2
     if is_square(p*(p-a)*(p-b)*(p-c)) and is_square(2*b**2+2*c**2-a**2) and is_square(2*a**2+2*c**2-b**2) and is_square(2*a**2+2*b**2-c**2) :
        print (a,b,c,2*b**2+2*c**2-a**2,2*a**2+2*c**2-b**2,2*a**2+2*b**2-c**2,p*(p-a)*(p-b)*(p-c))
