280cf34fa69c83a17ad623c987f576940f2dbde6
Поздняков Константин <fix@inbox.ru>
2021-01-17 15:49:42 +0500

    Update int_triangle.cpp

--- int_triangle.cpp
+++ int_triangle.cpp
@@ -12,21 +12,19 @@ bool isPerfectSquare(long double x)
   
 int main() { 
   long int p = 0;
-  for (int a = 3; a < 20000; a ++ )
-   for (int b = a; b < a+a+a; b ++ )
-    for (int c = b; c < b+b+b; c ++ )
-
+  for(int a = 3; a < 20000; a++ )
+   for(int b = a; b < a+a+a; b++)
+    for(int c = b; c < b+b+b; c++)
       p=(a+b+c)/2;
-      if (is_perfect_square(p*(p-a)*(p-b)*(p-c)) && ((is_perfect_square(2*b**2+2*c**2-a**2) | True) && is_perfect_square(2*a**2+2*c**2-b**2) && is_perfect_square(2*a**2+2*b**2-c**2)) )
-      
+      if (is_perfect_square(p*(p-a)*(p-b)*(p-c)) && ((is_perfect_square(2*b*b+2*c*c-a*a) | True) && is_perfect_square(2*a*a+2*c*c-b*b) && is_perfect_square(2*a*a+2*b*b-c*c)) )
       {
        cout << a;
        cout << b;
        cout << c;
-       cout << (math.sqrt(2*b**2+2*c**2-a**2))/2;
-       cout << (math.sqrt(2*a**2+2*c**2-b**2))/2;
-       cout << (math.sqrt(2*a**2+2*b**2-c**2))/2;
-       cout << math.sqrt((p*(p-a)*(p-b)*(p-c))));
+       cout << (sqrt(2*b**2+2*c**2-a**2))/2;
+       cout << (qrt(2*a**2+2*c**2-b**2))/2;
+       cout << (sqrt(2*a**2+2*b**2-c**2))/2;
+       cout << sqrt((p*(p-a)*(p-b)*(p-c))));
       }
 
   return 0; 
