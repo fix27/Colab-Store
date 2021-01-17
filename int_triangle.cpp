7ed8833a70e9c157d5e9c13f10c3655c9498cca6
Поздняков Константин <fix@inbox.ru>
2021-01-17 16:46:34 +0500

    Update int_triangle.cpp

--- int_triangle.cpp
+++ int_triangle.cpp
@@ -10,11 +10,12 @@ bool isPerfectSquare(long long n){
 int main() { 
   long int p = 0;
   for(int a = 3; a < 20000; a++)
+  {
+   if (a % 500 == 0) cout << "a=" << a << std::endl;
    for(int b = a; b < a+a+a; b++)
     for(int c = b; c < b+b+b; c++)
      {
       p=(a+b+c)/2;
-      if (a % 500 == 0) cout << "a=" << a << std::endl;
       if (isPerfectSquare(p*(p-a)*(p-b)*(p-c)) && isPerfectSquare(2*b*b+2*c*c-a*a) &&  isPerfectSquare(2*a*a+2*c*c-b*b) &&  isPerfectSquare(2*a*a+2*b*b-c*c))
       {
        std::cout  << a << " ";
@@ -25,6 +26,7 @@ int main() {
        std::cout  << (sqrt(2*a*a+2*b*b-c*c))/2 << " ";
        std::cout  << sqrt((p*(p-a)*(p-b)*(p-c))) << std::endl;
       }
+     }
     }
   return 0; 
 } 
