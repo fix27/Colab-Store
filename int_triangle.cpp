6124ed91312a229c182fcffc972a3892811e70e8
Поздняков Константин <fix@inbox.ru>
2021-01-24 10:21:31 +0500

    Update int_triangle.cpp

--- int_triangle.cpp
+++ int_triangle.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
          p+=0.5;
          if (isPerfectSquare(p*(p-a)*(p-b)*(p-c)) && isPerfectSquare(2*b*b+2*c*c-a*a) && isPerfectSquare(2*a*a+2*c*c-b*b) &&  isPerfectSquare(2*a*a+2*b*b-c*c))
           {
-           std::cout << a << " " b << " " c << " ";
+           std::cout << a << " " << b << " " << c << " ";
            std::cout << (sqrt(2*b*b+2*c*c-a*a))/2 << " " << (sqrt(2*a*a+2*c*c-b*b))/2 << " " << (sqrt(2*a*a+2*b*b-c*c))/2 << " ";
            std::cout << sqrt((p*(p-a)*(p-b)*(p-c))) << std::endl;
           }
