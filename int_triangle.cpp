98d7a1f11735a2f8b0d28e939b32fc76f84b81a8
Поздняков Константин <fix@inbox.ru>
2021-01-24 14:31:55 +0500

    Update int_triangle.cpp

--- int_triangle.cpp
+++ int_triangle.cpp
@@ -6,9 +6,9 @@ int main(int argc, char* argv[]) {
   cout.precision(20);
   if (argc > 1)// если передаем аргументы, то argc будет больше 1(в зависимости от кол-ва аргументов)
   {
-      long long ar1 = stoi(argv[1]);
-      long long ar2 = stoi(argv[2]);
-      long long ar3 = stoi(argv[3]);
+      long long ar1 = stoi(argv[1]); // 1
+      long long ar2 = stoi(argv[2]); // 1000
+      long long ar3 = stoi(argv[3]); // 5000
       long double intpart;
       long double p;
       for(long long  a = ar1 ; a < ar2; a++)
