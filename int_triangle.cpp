47e805b7881960124c3dd4bc04ad4297b1c374a9
Поздняков Константин <fix@inbox.ru>
2021-03-31 22:55:11 +0500

    Update int_triangle.cpp

--- int_triangle.cpp
+++ int_triangle.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* argv[]) {
         for(long long c = b; c < a+b; c++)
         {
          //p+=0.5;
-         std::cout << sqr16(c);
+         std::cout << sqr16(c) << " " << c << " ";
          if  (  modfl(sqrt(0 - (a - b - c) * (a + b - c) * (a - b + c) * (a + b + c)), &intpart) == 0.0 
              && modfl(sqrt(2*b*b+2*c*c-a*a), &intpart) == 0.0 
              && modfl(sqrt(2*a*a+2*c*c-b*b), &intpart) == 0.0 
