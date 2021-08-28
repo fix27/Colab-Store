ab3251e6d6891c213c4c92971369322c090f08fa
Поздняков Константин <fix@inbox.ru>
2021-08-29 03:34:45 +0500

    Add files via upload

---
+++ TestiNET.ipynb.sh
@@ -0,0 +1,56 @@
+# Author: fix@inbox.ru 
+# donate https://yasobe.ru/na/razvitie_po
+# Start date: 07.04.2020
+# Update date: 28.10.2020
+#pip install fast-cli
+#sudo apt install nethogs
+#sudo nethogs
+#sudo apt install nload
+#nload
+apt-get install bc
+curl -s dl.getipaddr.net/speedtest.sh -o- | bash
+sudo apt-get install npm node-gyp nodejs-dev libssl1.0-dev
+sudo npm update -g npm
+sudo npm install --global fast-cli
+fast -u
+sudo apt install youtube-dl
+youtube-dl -f best --no-part --no-cache-dir -o /dev/null --newline https://youtu.be/vSLxwOfJyQs
+#fast
+pip install speedtest-cli
+speedtest-cli
+sudo apt-get install iperf3
+iperf3 -c bouygues.iperf.fr -p 9127
+iperf3 -c bouygues.iperf.fr -p 9222
+iperf3 -c speedtest.serverius.net -p 5002
+iperf3 -c iperf.eenet.ee
+iperf3 -c iperf.it-north.net -p 5205
+iperf3 -c iperf.biznetnetworks.com -p 5201 
+iperf3 -c iperf.scottlinux.com -p 5201 	
+iperf3 -c iperf.he.net -p 5201 
+iperf3 -c rrru.ru
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate  https://cache-mskm905.cdn.yandex.net/internetometr.download.cdn.yandex.net/bigfile.png?1111111111111
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.tele2.net/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest6.tele2.net/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate  https://speedtest.serverius.net/files/100mb.bin
+wget --tries=1 -O /dev/null -q --show-progress http://ipv4.download.thinkbroadband.com/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress http://ipv6.download.thinkbroadband.com/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress http://download.thinkbroadband.com/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest-ny.turnkeyinternet.net/100mb.bin
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.ftp.otenet.gr/files/test100Mb.db
+wget --tries=1 -O /dev/null -q --show-progress http://ovh.net/files/100Mio.dat
+wget --tries=1 -O /dev/null -q --show-progress http://speedcheck.cdn.on.net/100meg.test
+wget --tries=1 -O /dev/null -q --show-progress http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.34952033.1572673891.1586208526-1150274045.1586208526
+wget --tries=1 -O /dev/null -q --show-progress http://212.183.159.230/100MB.zip
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.belwue.net/100M
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest-sgp1.digitalocean.com/100mb.test
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.as5577.net/100mb.bin
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.sea01.softlayer.com/downloads/test100.zip
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate https://yandex.ru/internet/
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate https://cache-mskstoredata03.cdn.yandex.net/internetometr.download.cdn.yandex.net/bigfile.png?1586248555096
+wget --tries=1 -O /dev/null -q --show-progress http://ip.mail.ru/
+wget --tries=1 -O /dev/null -q --show-progress http://ip.mail.ru/files/2mb.png?rnd=0.34026986463155295&t=&_=1586248874132
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest.newark.linode.com/100MB-newark.bin
+wget --tries=1 -O /dev/null -q --show-progress http://speedtest-blr1.digitalocean.com/10mb.test
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate  https://mc.yandex.ru/pixel/4436647312949346741?rnd=0.5
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate https://yadi.sk/i/DPZVlvZQHarJBg
+wget --tries=1 -O /dev/null -q --show-progress --no-check-certificate https://ipv6test.google.com
