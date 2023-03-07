27dbd3f8f0c707a82e2450dc05787bff76dd70ef
Поздняков Константин <fix@inbox.ru>
2023-03-08 00:41:19 +0500

    Update int_triangle2.cpp

--- int_triangle2.cpp
+++ int_triangle2.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    for (int a = 1; a <= 10000; a++) {
-        for (int b = a; b <= 10000; b++) {
+    for (int a = 1; a <= 100900; a++) {
+        for (int b = a; b <= 109000; b++) {
             for (int c = b+1; c < a+b; c++) {
-                int s = (a + b + c) / 2; // полупериметр
-                int medA = sqrt(2 * b * b + 2 * c * c - a * a) / 2; // медиана, проведенная из вершины A
-                int medB = sqrt(2 * a * a + 2 * c * c - b * b) / 2; // медиана, проведенная из вершины B
-                int medC = sqrt(2 * a * a + 2 * b * b - c * c) / 2; // медиана, проведенная из вершины C
-                int area = sqrt(s * (s - a) * (s - b) * (s - c)); // площадь треугольника
-                int meds = (medA + medB + medC) / 2;
+                unsigned long long s = (a + b + c) / 2; // полупериметр
+                unsigned long long medA = sqrt(2 * b * b + 2 * c * c - a * a) / 2; // медиана, проведенная из вершины A
+                unsigned long long medB = sqrt(2 * a * a + 2 * c * c - b * b) / 2; // медиана, проведенная из вершины B
+                unsigned long long medC = sqrt(2 * a * a + 2 * b * b - c * c) / 2; // медиана, проведенная из вершины C
+                unsigned long long area = sqrt(s * (s - a) * (s - b) * (s - c)); // площадь треугольника
+                unsigned long long meds = (medA + medB + medC) / 2;
                 if ((area*area) == (meds * (meds - medA) * (meds - medB) * (meds - medC))*9/16// проверка, что это треугольник с медианами
                     ) // проверка, что площадь кратна 6 area
                 {
