cae9a8913ae37d8667dc4dbad55adfbae660758e
Поздняков Константин <fix@inbox.ru>
2023-03-08 03:38:45 +0500

    Update int_triangle2.cpp

--- int_triangle2.cpp
+++ int_triangle2.cpp
@@ -7,17 +7,19 @@ int main(int argc, char* argv[]) {
         for (long long b = a ; b <= 109000; b++) {
             for (long long c = b+1 ; c < a+b-2; c++) {
                 long long s = (a + b + c) / 2; // полупериметр
-                //long long area = sqrt(s * (s - a) * (s - b) * (s - c)); // площадь треугольника                
-                long long medA = sqrt(2 * b * b + 2 * c * c - a * a) / 2; // медиана, проведенная из вершины A
-                long long medB = sqrt(2 * a * a + 2 * c * c - b * b) / 2; // медиана, проведенная из вершины B
-                long long medC = sqrt(2 * a * a + 2 * b * b - c * c) / 2; // медиана, проведенная из вершины C
-                long long meds = (medA + medB + medC) / 2;
-                if ( (s * (s - a) * (s - b) * (s - c))* 9 == (meds * (meds - medA) * (meds - medB) * (meds - medC))*16)
-                {
-                    cout << "a,b,c: " << a << ", " << b << ", " << c << endl;
-                    cout << "Медианы: " << medA << ", " << medB << ", " << medC << endl;
-                    cout << "S: " << sqrt(s * (s - a) * (s - b) * (s - c)) << endl;
-                }
+                long long area2 = s * (s - a) * (s - b) * (s - c)
+                long long area = sqrt(area2); // площадь треугольника
+                if ( area*area=area2)
+                    long long medA = sqrt(2 * b * b + 2 * c * c - a * a) / 2; // медиана, проведенная из вершины A
+                    long long medB = sqrt(2 * a * a + 2 * c * c - b * b) / 2; // медиана, проведенная из вершины B
+                    long long medC = sqrt(2 * a * a + 2 * b * b - c * c) / 2; // медиана, проведенная из вершины C
+                    long long meds = (medA + medB + medC) / 2;
+                    if ( area2*9 == (meds * (meds - medA) * (meds - medB) * (meds - medC))*16)
+                    {
+                        cout << "a,b,c: " << a << ", " << b << ", " << c << endl;
+                        cout << "Медианы: " << medA << ", " << medB << ", " << medC << endl;
+                        cout << "S: " << sqrt(s * (s - a) * (s - b) * (s - c)) << endl;
+                    }
             }
         }
     }
