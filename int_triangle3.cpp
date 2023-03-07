c9894bf2604226c0d5eaabbff2cb42ed44966339
Поздняков Константин <fix@inbox.ru>
2023-03-08 03:12:36 +0500

    Update int_triangle3.cpp

--- int_triangle3.cpp
+++ int_triangle3.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
     for (long long a = 1; a <= 100900; a++) {
         for (long long b = a ; b <= 109000; b++) {
             for (long long c = b+1 ; c < a+b-2; c++) {
-                //long long area = sqrt(s * (s - a) * (s - b) * (s - c)); // площадь треугольника                
+                        
                 long long medA = sqrt(2 * b * b + 2 * c * c - a * a) / 2; // медиана, проведенная из вершины A
                 long long medB = sqrt(2 * a * a + 2 * c * c - b * b) / 2; // медиана, проведенная из вершины B
                 long long medC = sqrt(2 * a * a + 2 * b * b - c * c) / 2; // медиана, проведенная из вершины C
@@ -14,11 +14,12 @@ int main(int argc, char* argv[]) {
                 {
                     long long s = (a + b + c) / 2; // полупериметр
                     long long meds = (medA + medB + medC) / 2;
-                    if ( (s * (s - a) * (s - b) * (s - c))*9 == (meds * (meds - medA) * (meds - medB) * (meds - medC))*16)
+                    long long area = sqrt(s * (s - a) * (s - b) * (s - c)); // площадь треугольника      
+                    if ( area*area*9 == (meds * (meds - medA) * (meds - medB) * (meds - medC))*16)
                     {   
                        cout << "a,b,c: " << a << ", " << b << ", " << c << endl;
                        cout << "Медианы: " << medA << ", " << medB << ", " << medC << endl;
-                       cout << "S: " << sqrt(s * (s - a) * (s - b) * (s - c)) << endl;
+                       cout << "S: " << area) << endl;
                     }
 
                 }
